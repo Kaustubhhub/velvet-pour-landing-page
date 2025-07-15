854b17aa1060d9ca95a88d67324f6ce7bbc5e80f
kaustubh <kaustubhkumbhare02@gmail.com>
2025-07-15 06:51:09 +0000

    implemented navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import Navbar from './components/Navbar'
+import gsap from 'gsap'
+import { ScrollTrigger, SplitText } from 'gsap/all'
+gsap.registerPlugin(ScrollTrigger, SplitText)
+
 
 function App() {
   return (
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,45 @@
-const Navbar = () =>{
-    return(
-        <div>
-            navbar
-        </div>
+import { useGSAP } from "@gsap/react"
+import { navLinks } from "../constants"
+import gsap from "gsap"
+
+useGSAP(() => {
+    const navTween = gsap.timeline({
+        scrollTrigger: {
+            trigger: 'nav',
+            start: 'bottom top'
+        },
+    })
+
+    navTween.fromTo('nav', {
+        backgroundColor: 'transparent'
+    }, {
+        backgroundColor: '#00000050',
+        backgroundFilter: 'blue(10px)',
+        duration: '1',
+        ease: 'power1.inOut'
+    })
+})
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div>
+                <a href="#home" className="flex item-center gap-2">
+                    <p>
+                        Velvet Pour
+                    </p>
+                </a>
+                <ul>
+                    {navLinks.map(link => (
+                        <li key={link.id}>
+                            <a href={`#${link.id}`}>
+                                {link.title}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </nav>
     )
 }
 

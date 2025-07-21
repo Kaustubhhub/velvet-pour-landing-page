253dffd8c714c2f74b0d8a3eecd32cc0af40f311
kaustubh <kaustubhkumbhare02@gmail.com>
2025-07-21 17:44:24 +0000

    added about section

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import gsap from 'gsap'
 import { ScrollTrigger, SplitText } from 'gsap/all'
 import Hero from './components/Hero'
 import Cocktails from './components/Cocktails'
+import About from './components/About'
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
 
@@ -10,9 +11,10 @@ function App() {
   return (
     <main>
       <Navbar />
-      <Hero/>
-      <Cocktails/>
-     </main>
+      <Hero />
+      <Cocktails />
+      <About />
+    </main>
   )
 }
 
